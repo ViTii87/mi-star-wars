937602d70135320cec24fd7d2a98cdcd0c20bc6b
Victor <victormel87@gmail.com>
2016-04-01 12:49:34 +0200

    Cambiado el programa para que tome los datos del metodo y no del constructor

--- NameGenerator.java
+++ NameGenerator.java
@@ -7,29 +7,21 @@
  */
 public class NameGenerator
 {
-    private String tuNombre;
-    private String tuApellido;
-    private String apellidoMadre;
-    private String ciudadNacimiento;
-    
     private static final int PRIMERAS_LETRAS = 3;
     private static final int PRIMERAS_LETRAS_MADRE = 2;
     
     /**
      * Constructor for objects of class NameGenerator
      */
-    public NameGenerator(String tuNombre, String tuApellido, String apellidoMadre, String ciudadNacimiento)
+    public NameGenerator()
     {
-       this.tuNombre = tuNombre;
-       this.tuApellido = tuApellido;
-       this.apellidoMadre = apellidoMadre;
-       this.ciudadNacimiento = ciudadNacimiento;
+ 
     }
 
     /**
      * Metodo que generara tu nombre de forma aleatoria
      */
-    public String generateStarWarsName(){
+    public String generateStarWarsName(String tuNombre, String tuApellido, String apellidoMadre, String ciudadNacimiento){
         String nombreDePelicula = null;
         nombreDePelicula = tuApellido.substring(0, PRIMERAS_LETRAS) 
         + tuNombre.substring(0, PRIMERAS_LETRAS).toLowerCase()
